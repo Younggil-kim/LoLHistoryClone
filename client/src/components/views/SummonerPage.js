2cd25fc641f29770ce091293e61fa21c10d07320
김영길 <gom991@ajou.ac.kr>
2021-02-23 14:38:14 +0900

    소환사페이지에서 정보 받아오기

--- client/src/components/views/SummonerPage.js
+++ client/src/components/views/SummonerPage.js
@@ -6,46 +6,54 @@ const SummonerPage = ({location, match}) => {
     const query = queryString.parse(location.search);
     // console.log(query)
     const [summonerLevel, setsummonerLevel] = useState("")
+    const [accountId, setaccountId] = useState("")
+    const [summonerId, setsummonerId] = useState("")
+    const [tier, settier] = useState("")
     // console.log("검색하신 소환사 명은 ",query)
-    
-    const body = new Promise((resolve, reject) => {
+    // 받아와야할게 matchid => 이거로 최근 20개 전적을 뽑아와서, 뽑아낸 matchid로 그 게임 상세 정보를 가져오기
+    // 
+    const summonerV4 = new Promise((resolve, reject) => {
         try{
-            const request = axios.post('/api/LandingPage', query)
+            const request = axios.post('/api/LandingPage/summoner/v4', query)
                 .then(response => {return response.data})
+
             resolve(request)
             
         } catch(err){
             reject(new Error(err))
         }
     })
-    body.then((data) => {
+    summonerV4.then((data) => {
         console.log(data)
         setsummonerLevel(data.searchData.summonerLevel)
-    })
-
-    // const request = axios.post('/api/LandingPage', query)
-    //     .then(response => response.data)
-        
-        // {
-        //     if(response.data.searchSuccess){
-        //         response.data.searchData
-        //         // console.log(response.data.searchData)
-        //         // console.log(response.data.searchData.name)
-        //         // let data = response.data.searchData
-        //         // console.log("검색은",data)
-        //     } else{
-        //         response.data.searchSuccess
-        //         alert(`${response.data.message}`)
-        //     }
-        // })
-        
+        setaccountId(data.searchData.accountId)
+        setsummonerId(data.searchData.id)
+        let id = {
+            summonerId : data.searchData.id
+        }
+        const leagueV4 = new Promise((resolve, reject) => {
+            try{
+                const request = axios.post("/api/LandingPage/league/v4", id)
+                    .then(response => {return response.data})
+                resolve(request)
+            } catch(err){
+                reject(new Error(err))
+            }
+        })
+        leagueV4.then((data) => {
+            console.log(data)
+            settier(data.searchData[0].tier)
+        })
+    })  
 
     return (
         <div>
             <h2>{query.name}</h2>
             님의 소환사페이지입니다.
             <br></br>
-    소환사님의 레벨은 {summonerLevel && <div>{summonerLevel} 입니다.</div>}
+            소환사님의 레벨은 {summonerLevel && <span>{summonerLevel} 입니다.</span>}
+            <br></br>
+            소환사님의 티어는 {tier && <span>{tier} 입니다.</span>}
         </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const API_URI = 'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
-const API_KEY = 'RGAPI-82f4758c-58b6-43b7-898b-70109129867c';
+const API_KEY = 'RGAPI-eb1d5cdc-32df-44bb-a363-d7d64e3e77c6';
 const name = 'hide on bush'
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
@@ -14,14 +14,10 @@ app.get('/', (req, res) => {
     res.send('서버생성됨')
 })
 
-app.get('/api/hello', (req, res) => {
-    res.send('안녕하세요')
-})
-
-app.post('/api/LandingPage', async (req, res) =>{
+app.post('/api/LandingPage/summoner/v4', async (req, res) =>{
     var name = req.body.name;
     name = encodeURI(name)
-    console.log(req.body.name)
+    console.log(req.body)
     try{
         const { data } = await axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${API_KEY}`);
         return res.json({
@@ -34,13 +30,26 @@ app.post('/api/LandingPage', async (req, res) =>{
             message: "소환사명을 찾을 수 없습니다."
         })
     }
-
-    // return res.json({
-    //     searchSuccess: true,
-    //     message: "search에 성공함",
-    //     data : data
-    // })
 })
+
+app.post('/api/LandingPage/league/v4', async(req, res) => {
+    id = req.body.summonerId
+    console.log(id)
+    try{
+        const { data } = await axios.get(`https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${API_KEY}`);
+        return res.json({
+            searchSuccess: true,
+            searchData: data
+        })
+    } catch (error){
+        return res.json({
+            searchSuccess: false,
+            message: "티어 정보를 찾을 수 없습니다."
+        })
+    }
+})
+
+
 
 
 app.listen(port, () => {
